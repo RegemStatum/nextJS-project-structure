9513c1913e0fa88d785f1780077a62a534f772a1
Oleksandr Kondratov <al.kon.workmail@gmail.com>
2022-03-09 14:47:01 +0200

    +

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,46 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          {/* preloading fonts */}
+          <link
+            rel="preload"
+            href="/fonts/Roboto/Roboto-Regular.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/Roboto/Roboto-Medium.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/Roboto/Roboto-Black.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/OpenSans/OpenSans-Regular.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/OpenSans/OpenSans-Medium.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/OpenSans/OpenSans-ExtraBold.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          {/* end of preloading fonts */}
+        </Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,31 @@
 import Link from "next/link";
+import Image from "next/image";
 import React, { FC } from "react";
 import styles from "../styles/pages/home.module.scss";
+
+import cocktail1 from "../public/images/cocktail1.jpg";
 
 const Index: FC = () => {
   return (
     <div className={styles.container}>
-      Hello from home{" "}
+      Hello from home
       <Link href="/products">
         <a>Products</a>
       </Link>
+      <Image
+        src={cocktail1}
+        alt="my favourite cocktail"
+        placeholder="blur"
+        priority
+      />
+      <Image
+        src="/images/cocktail2.jpg"
+        alt="cocktail"
+        width="130"
+        height="130"
+        placeholder="blur"
+        blurDataURL="/images/cocktail2.jpg"
+      />
     </div>
   );
 };
